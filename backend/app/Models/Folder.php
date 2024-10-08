6cba55dec7912d40076132eb055c32a37b8521a7
tomitaka-bizmates <rintaro.tomitaka@bizmates.co.jp>
2024-10-08 14:00:06 +0900

    Get Folder Info

--- backend/app/Models/Folder.php
+++ backend/app/Models/Folder.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Folders extends Model
+class Folder extends Model
 {
     use HasFactory;
 }
